72f85030c00eb60a01d564d5202eab45f1b5b8a2
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-01-24 23:34:48 +0300

    feat: adjusting custom hook logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
@@ -7,7 +7,7 @@ import useHttp from "./hooks/use-http";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (tasksObk) => {
+  const transformTasks = useCallback((tasksObk) => {
     const loadedTasks = [];
 
     for (const taskKey in tasksObk) {
@@ -15,18 +15,15 @@ function App() {
     }
 
     setTasks(loadedTasks);
-  };
-
-  const {isLoading, error, sendRequest: fetchTasks } = useHttp(
-    {
-      url: "https://todo-27646-default-rtdb.firebaseio.com/tasks.json",
-    },
-    transformTasks
-  );
-  
-  useEffect(() => {
-    fetchTasks();
   }, []);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
+
+  useEffect(() => {
+    fetchTasks({
+      url: "https://todo-27646-default-rtdb.firebaseio.com/tasks.json",
+    });
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
